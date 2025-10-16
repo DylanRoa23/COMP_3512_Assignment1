6b24aa2696dc2472e0cde27038e087809c190b45
Kyle Chen <150177160+Existenial@users.noreply.github.com>
2025-10-15 21:26:55 -0600

    Renamed Pdo.php and completed functions.

--- droa858/includes/pdo.php
+++ droa858/includes/pdo.php
@@ -6,13 +6,21 @@ class PDOControl {
     private const ERRMODE = PDO::ERRMODE_EXCEPTION;
 
     /* Fields */
-    private $connString;
-    private $user;
-    private $pass;
-    private $pdo;
+    private string $connString;
+    private string $user;
+    private string $pass;
+    private ?PDO $pdo;
 
-    /* Constructor */
-    public function __construct($connString, $user, $pass) {
+    /**
+     * Constructor
+     * 
+     * Initialize fields and connects to the database
+     * 
+     * @param string $connString Connection string
+     * @param string $user Username
+     * @param string $pass Password
+     */
+    public function __construct(string $connString, string $user, string $pass) {
 
         /* Save data */
         $this->connString = $connString;
@@ -24,11 +32,74 @@ class PDOControl {
             $pdo = new PDO($this->connString, $this->user, $this->pass);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, self::ERRMODE);
             $this->pdo = $pdo;
-        } catch (PDOException $e) {
+        }
+        catch (PDOException $e) {
             die("Connection failed: " . $e->getMessage());
         }
         
     }
+
+    /* Functions */
+
+    /**
+     * 
+     * Queries the database with an SQL string and parameters.
+     * 
+     * @param string $sql An SQL query string with named parameters.
+     *  Format: "SELECT * FROM table WHERE column = :paramName AND column2 = :paramName2"
+     * @param ?array $paramArray A parameter array or null if no parameters. 
+     *  Format: ['paramName' => 'paramValue', 'paramName2' => 'paramValue2']
+     * @return ?PDOStatement Returns a PDOStatement on success or null on failure.
+     */
+    public function query(string $sql, ?array $paramArray): ?PDOStatement {
+
+        /* Initialize. */
+        $statement = null;
+
+        /* Try */
+        try {
+
+            /* Prepare and execute. */
+            $statement = $this->pdo->prepare($sql);
+            $statement->execute($paramArray);
+
+        }
+        /* If failed, */
+        catch (PDOException $e){
+
+            /* Die */
+            die("Query failed: " . $e->getMessage());
+
+        }
+
+        /* Return */
+        return $statement;
+    }
+
+    /**
+     * 
+     * Closes the connection to the database. This Pdo instance is no longer usable.
+     * 
+     * @return void
+     */
+    public function close(): void {
+        
+        /* Try */
+        try {
+            
+            /* Close */
+            $this->pdo = null;
+
+        }
+        /* If failed, */
+        catch (PDOException $e) {
+
+            /* Die */
+            die("Close failed: " . $e->getMessage());
+
+        }
+    }
+
 
 }
 
