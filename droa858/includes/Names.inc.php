68f854945d0d204edc4441f34c581a2886eb566d
DylanRoa23 <dylanroa23@gmail.com>
2025-10-16 19:08:47 -0600

    making pdo for users names

--- droa858/includes/Names.inc.php
+++ droa858/includes/Names.inc.php
@@ -2,7 +2,24 @@
 /**
  * Fetches and displays user names from the stocks.db database.
  */
+class UserNames {
 
+    /**
+     * Returns all user names from the database.
+     * @return array An array of associative arrays containing user data.
+     */
+    public static function getAllUsers(): array {
+        $data = [];
 
+        $sql = "SELECT lastname, firstname 
+        FROM users";
+        $statement = PDOControl::query($sql);
 
+        if ($statement) {
+            $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        return $data;
+    }
+}
 ?>
