9f1a16572f0667c8e1ba7c73e3944b5bff589675
Existenial <150177160+Existenial@users.noreply.github.com>
2025-10-19 23:32:12 -0600

    Added some styling to about.

--- droa858/about.php
+++ droa858/about.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,32 +9,38 @@
     <link rel="stylesheet" href="css/about.css">
     <title>About</title>
 </head>
+
 <body>
     <?php require_once 'includes/header.inc.php' ?>
 
     <main>
         <h2>About</h2>
-    
+
         <p>
             This is our submission for our COMP3512 Assignment #1 at Mount Royal University. This assignment requires us
-            to demonstrate proficiency with HTML, CSS, PHP, SQL, PDO control, and databases. This assignment will measure
+            to demonstrate proficiency with HTML, CSS, PHP, SQL, PDO control, and databases. This assignment will
+            measure
             our skill with these languages and provide APIs for a future assignment.
         </p>
 
         <p>
-            This websites fetches the portfolio 
-            of every client in our database and displays the data with PHP and SQL in an easy-to-read format. This website has
-            various details on the customers' portfolios, such as the number of companies they have shares in, the total number
-            of shares and the total value of those shares. 
+            This websites fetches the portfolio
+            of every client in our database and displays the data with PHP and SQL in an easy-to-read format. This
+            website has
+            various details on the customers' portfolios, such as the number of companies they have shares in, the total
+            number
+            of shares and the total value of those shares.
         </p>
 
         <p>
             This work was done by Dylan Roa and Kyle Chen.
         </p>
-        <p> 
+        <p>
             Our Tools:
+            <br>
+            <a href="https://github.com/DylanRoa23/COMP_3512_Assignment1">Our GitHub Repository</a>
         </p>
-        <a href="https://github.com/DylanRoa23/COMP_3512_Assignment1">Our GitHub Repository</a>
     </main>
 </body>
+
 </html>
